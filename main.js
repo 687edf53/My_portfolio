2795219da5bc7c2658e5534fbf44232e8bb48c87
687edf53 <sm3257698@gmail.com>
2024-01-06 20:00:16 +0200

    Completed Update

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 // Elements
-const burgerMenu = document.querySelector('.burger-menu'),
-      navbarLinks = document.querySelector('.navbar ul');
+const burgerMenu = document.querySelector(".burger-menu"),
+  navbarLinks = document.querySelector(".navbar ul"),
+  navbar = document.querySelector(".navbar"),
+  sections = document.querySelectorAll("section");
 
 // Navbar
-burgerMenu.addEventListener('click', () => {
-  burgerMenu.classList.toggle('close');
-  navbarLinks.classList.toggle('visible')
-})
+function bugerMenuFunc() {
+  burgerMenu.classList.toggle("close");
+  navbarLinks.classList.toggle("visible");
+}
+
+function bugerMenuFuncClose() {
+  burgerMenu.classList.remove("close");
+  navbarLinks.classList.remove("visible");
+}
+
+navbarLinks.childNodes.forEach((link) =>
+    link.addEventListener("click", bugerMenuFuncClose));
+burgerMenu.addEventListener("click", bugerMenuFunc);
+sections.forEach((section) => {
+  section.addEventListener("click", bugerMenuFuncClose);
+});
